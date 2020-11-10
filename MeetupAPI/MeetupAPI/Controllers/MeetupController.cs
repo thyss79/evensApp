6055d9c7731ceff151803cdb061559b9e29f71ed
Piotr Kiliński <piotr.kilinski@ecom.software>
2020-11-10 16:43:00 +0100

    first controller

--- MeetupAPI/MeetupAPI/Controllers/MeetupController.cs
+++ MeetupAPI/MeetupAPI/Controllers/MeetupController.cs
@@ -3,12 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MeetupAPI.Entities;
 using Microsoft.AspNetCore.Mvc;
 
 namespace MeetupAPI.Controllers
 {
-    public class MeetupController : ControllerBase
-    {
-         
+    [Route("api/meetup")]
+    public class MeetupController : ControllerBase {
+
+        private readonly MeetupContext _meetupContext;
+        public MeetupController(MeetupContext meetupContext) {
+            _meetupContext = meetupContext;
+        }
+        [HttpGet]
+        public ActionResult<List<Meetup>> Get(){
+            var meetups = _meetupContext.Meetups.ToList();
+            
+            return Ok(meetups);
+        }
     }
 }
